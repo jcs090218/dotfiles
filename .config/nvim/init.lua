5a08604042c2a51c73638420e33d4dd00bf87e9a
Jen-Chieh Shen <jcs090218@gmail.com>
2025-11-13 00:00:12 +0800

    chore: more setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,55 @@
-vim.opt.relativenumber = true 
+-- init.lua
+
+vim.g.have_nerd_font = true
+
+-- Line Numbers
+--vim.o.number = true
+vim.opt.relativenumber = true
+
+vim.o.mouse = 'a'
+
+vim.o.showmode = true
+
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 1
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
