7f76626f41a1f5d3f10349ec44ce1041753cfda9
Jen-Chieh Shen <jcs090218@gmail.com>
2026-01-14 06:29:14 -0800

    Create samba.nix

---
+++ nix/internal/samba.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    # settings = ''
+    #   workgroup = SAMBA
+    #   server string = %h server (Samba %v)
+    #   netbios name = smbnix
+    #   security = user
+    #   #use sendfile = yes
+    #   #max protocol = smb2
+    #   # note: localhost is the ipv6 localhost ::1
+    #   hosts allow = 192.168.15. 127.0.0.1 localhost
+
+    #   hosts deny = 0.0.0.0/0
+    #   guest account = nobody
+    #   map to guest = bad user
+    # '';
+    settings = {
+      public = {
+        path = "/mnt/samba/Public";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "jenchieh";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+}
