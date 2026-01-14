a4823f89606b12e244e25e1524394935ed532700
Jen-Chieh Shen <jcs090218@gmail.com>
2026-01-14 06:27:55 -0800

    Add desktop configuration for NixOS

---
+++ nix/internal/desktop.nix
@@ -0,0 +1,134 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.config.allowBroken = true;
+
+  environment.systemPackages = with pkgs;
+    [
+      betterlockscreen
+      # master.code-cursor
+      dmenu
+      dunst
+      # font-manager
+      gimp
+      google-chrome
+      gparted
+      inkscape
+      (flameshot.override { enableWlrSupport = true; })
+      libinput-gestures
+      libnotify
+      libcanberra-gtk3
+      materia-theme
+      nautilus
+      ntfsprogs
+      pavucontrol
+      peek
+      waybar
+      playerctl
+      pulsemixer
+      rofi
+      kanshi
+      slurp
+      sound-theme-freedesktop
+      swaybg
+      udiskie
+      udisks
+      transmission_4-gtk
+      vlc
+      woeusb
+      wf-recorder
+      adwaita-icon-theme
+    ];
+
+  environment.variables = {
+    DOTFILES = "$HOME/.dotfiles";
+  };
+
+  environment.etc."libinput-gestures.conf".text = ''
+    gesture swipe right 3 bspc desktop -f next.local
+    gesture swipe left 3 bspc desktop -f prev.local
+  '';
+
+  programs.hyprland = {
+    enable = true;
+    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    xwayland.enable = true;
+  };
+
+  services = {
+    upower.enable = true;
+    acpid.enable = true;
+    gnome.gnome-keyring.enable = true;
+
+    libinput.enable = true;
+    libinput.touchpad.naturalScrolling = true;
+    displayManager.defaultSession = "hyprland";
+
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = true;
+
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us,br";
+        variant = "intl,abnt2";
+      };
+
+      videoDrivers = [ "modesetting" ];
+      # desktopManager.gnome.enable = true;
+      desktopManager.xterm.enable = false;
+      #displayManager.startx.enable = true;
+
+      #displayManager.lightdm = {
+      #  enable = true;
+      #  greeters.mini = {
+      #    enable = true;
+      #    user = "jenchieh";
+      #    extraConfig = ''
+      #          [greeter]
+      #          show-password-label = false
+      #          show-input-cursor = true
+      #          invalid-password-text = "You shall not pass!"
+      #          password-alignment = left
+      #          cursor-theme-name = "Adwaita"
+      #          [greeter-theme]
+      #          background-image = ""
+      #          background-color = "#282a36"
+      #          window-color = "#bd93f9"
+      #          border-color = "#bd93f9"
+      #          layout-space = 16
+      #          font-size = 1.1em
+      #          password-background-color = "#44475a"
+      #          password-border-width = 4px
+      #          password-border-color = "#44475a"
+      #      '';
+      #  };
+      #};
+
+      #xautolock = {
+      #  enable = true;
+      #  enableNotifier = true;
+      #  locker = "${pkgs.betterlockscreen}/bin/betterlockscreen -s";
+      #  notify = 40;
+      #  notifier = ''${pkgs.libnotify}/bin/notify-send -c "lockscreen"  "Locking in 40 seconds"'';
+      #  extraOptions = [ "-corners +00-" ];
+      #};
+    };
+
+    # logind.extraConfig = ''
+    #   HandlePowerKey=suspend-then-hibernate
+    # '';
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      emacs-all-the-icons-fonts
+      hack-font
+      roboto
+      roboto-mono
+      master.material-design-icons
+      ibm-plex
+    ];
+  };
+
+}
