e5bf89342c11e9cd30f86b85c1de3cdc0a6dba85
JenChieh <jcs090218@gmail.com>
2026-02-04 01:10:03 +0800

    add init

---
+++ .emacs.jcs.d/init.el
@@ -0,0 +1,10 @@
+;;; init.el --- Your own configuration  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(setq user-full-name "Jen-Chieh Shen"
+      user-mail-address "jcs090218@gmail.com")
+
+;; Put your configuration code here ...
+
+;;; init.el ends here
