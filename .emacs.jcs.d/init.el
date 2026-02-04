b790e5508ede0fda4c434b7f42ce73b0ad13a63a
Jen-Chieh Shen <jcs090218@gmail.com>
2026-02-03 23:16:23 -0800

    feat: Add mail settings

--- .emacs.jcs.d/init.el
+++ .emacs.jcs.d/init.el
@@ -5,6 +5,13 @@
 (setq user-full-name "Jen-Chieh Shen"
       user-mail-address "jcs090218@gmail.com")
 
-;; Put your configuration code here ...
+;;
+;;; Mail
+
+(setq message-send-mail-function #'smtpmail-send-it)
+
+(setq send-mail-function #'smtpmail-send-it
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587)  ; 456 for SSL / 587 for TLS
 
 ;;; init.el ends here
