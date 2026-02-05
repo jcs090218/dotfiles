5a4d361eeaee4eaa8cedadc5e6e2788ca759c327
jcs090218@gmail.com <jcs090218@gmail.com>
2026-02-05 13:48:58 +0800

    fix: send mail require user

--- .emacs.jcs.d/init.el
+++ .emacs.jcs.d/init.el
@@ -13,6 +13,7 @@
   (setq message-send-mail-function #'smtpmail-send-it)
 
   (setq send-mail-function #'smtpmail-send-it
+        smtpmail-smtp-user user-mail-address
         smtpmail-default-smtp-server "smtp.gmail.com"
         ;; 456 for SSL / 587 for TLS
         smtpmail-smtp-service 587))
